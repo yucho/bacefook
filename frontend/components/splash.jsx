2ca4e25a0759c61c2c8cab6abb5aa966fa86e00d
yucho <yucho.ho@gmail.com>
2019-05-10 10:53:09 -0700

    Fake splash menu

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -42,10 +42,41 @@ const Splash = props => {
       <footer className="splash-outer splash-footer">
         <ul className="splash-footer-locale">
           <li>English (US)</li>
+          <li><Link to="/">Español</Link></li>
+          <li><Link to="/">Français (France)</Link></li>
+          <li><Link to="/">中文(简体)</Link></li>
+          <li><Link to="/">العربية</Link></li>
+          <li><Link to="/">Português (Brasil)</Link></li>
+          <li><Link to="/">Italiano</Link></li>
+          <li><Link to="/">한국어</Link></li>
+          <li><Link to="/">Deutsch</Link></li>
+          <li><Link to="/">हिन्दी</Link></li>
+          <li><Link to="/">日本語</Link></li>
         </ul>
         <ul className="splash-footer-nav">
           <li><Link to="/">Sign Up</Link></li>
           <li><Link to="/">Log In</Link></li>
+          <li><Link to="/">Messenger</Link></li>
+          <li><Link to="/">Facebook Lite</Link></li>
+          <li><Link to="/">Find Friends</Link></li>
+          <li><Link to="/">People</Link></li>
+          <li><Link to="/">Profiles</Link></li>
+          <li><Link to="/">Pages</Link></li>
+          <li><Link to="/">Page Categories</Link></li>
+          <li><Link to="/">Places</Link></li>
+          <li><Link to="/">Games</Link></li>
+          <li><Link to="/">Locations</Link></li>
+          <li><Link to="/">Marketplace</Link></li>
+          <li><Link to="/">Groups</Link></li>
+          <li><Link to="/">Instagram</Link></li>
+          <li><Link to="/">Local</Link></li>
+          <li><Link to="/">Fundraisers</Link></li>
+          <li><Link to="/">About</Link></li>
+          <li><Link to="/">Create Ad</Link></li>
+          <li><Link to="/">Create Page</Link></li>
+          <li><Link to="/">Developers</Link></li>
+          <li><Link to="/">Careers</Link></li>
+          <li><Link to="/">Privacy</Link></li>
         </ul>
         <small className="splash-footer-copyright">Bacefook &#9728; 2019</small>
       </footer>
