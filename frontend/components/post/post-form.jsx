63a535b5dc8ebe4dea38ccce8eff9cffe7ba7d91
yucho <yucho.ho@gmail.com>
2019-06-23 23:16:34 -0700

    Install react-textarea-autosize

--- frontend/components/post/post-form.jsx
+++ frontend/components/post/post-form.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Prompt } from 'react-router-dom'
+import Textarea from 'react-textarea-autosize';
 import { createPost } from 'actions/posts-actions';
 
 const PostForm = ({postable_id = null, postable_type = 'User'}) => {
@@ -12,9 +14,8 @@ const PostForm = ({postable_id = null, postable_type = 'User'}) => {
       <h3>Create Post</h3>
       <form className="post-form" onSubmit={submitPost(dispatch, body, postable_id, postable_type, setBody)}>
         <div className="post-form-circular-image"/>
-        <input onChange={handleUpdate(setBody)} type="text" value={body}
-          placeholder="What's on your mind?"
-        />
+        <Textarea onChange={handleUpdate(setBody)} value={body} placeholder="What's on your mind?" />
+        <Prompt when={!!body} message="You haven't finished your post yet. Do you want to leave without finishing?" />
         <input type="submit" value="Share" className="post-form-share-button" />
       </form>
     </section>
