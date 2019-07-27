789a7a62dcca040382adc099f074a3ba8ada6aaa
yucho <yucho.ho@gmail.com>
2019-07-26 19:37:56 -0700

    Bugfix out-of-sync like style like button

--- frontend/components/post/post-like-comment.jsx
+++ frontend/components/post/post-like-comment.jsx
@@ -4,49 +4,58 @@ import { PostShowContext } from './post-show';
 import { createLike, destroyLike } from 'actions/likes-actions';
 import { fetchPost } from 'actions/posts-actions';
 
-const assign = (obj) => (prev) => Object.assign({}, prev, obj);
-
 const PostLikeComment = () => {
-  const {post} = useContext(PostShowContext);
-  const [state, setState] = useState({ you: null });
+  const [clicked, setClicked]             = useState(false);
+  const [localReaction, setLocalReaction] = useState(null);
 
-  const [myUserId, allLikes] = useSelector((state) => [state.session.id, state.likes]);
-  const likes = post.likes.map((id) => allLikes[id]).filter((like) => !!like);
-  const myLike = likes.find((like) => like.user_id === myUserId);
-
-  useEffect(() => {
-    const reaction = myLike ? myLike.reaction : null;
-    setState(assign({ you: reaction }));
-  }, [likes]);
-
-  const dispatch = useDispatch();
   const toggleLike = (reaction) => () => {
-    if (state.you && !reaction) {
-      if(myLike) dispatch(destroyLike(myLike));
-      setState(assign({ you: null }));
+    const newReaction = reaction || 'like';
+    if (!reaction) {
+      setLocalReaction(prev => !!prev ? null : newReaction);
     } else {
-      const newReaction = reaction || 'like';
-      let task;
-      if (myLike) {
-        task = destroyLike(myLike)(dispatch);
-      } else {
-        task = Promise.resolve();
-      }
-      task.then(() => dispatch(createLike({
-        like: {
-          likeable_id: post.id,
-          likeable_type: 'Post',
-          reaction: newReaction
-        }
-      }))).then(() => dispatch(fetchPost(post.id)));
-      setState(assign({ you: reaction }));
+      setLocalReaction(newReaction);
     }
+    if (!clicked) setClicked(true);
   };
+
+  const post = useContext(PostShowContext).post;
+  const likes = useSelector((state) => state.likes);
+  const userId = useSelector((state) => state.session.id);
+  const likeId = post.likes.find((id) => likes[id] && likes[id].user_id === userId);
+
+  const initLocalReaction = () => {
+    if (!clicked && likeId) setLocalReaction(likes[likeId].reaction);
+  };
+  useEffect(initLocalReaction, [post, likes]);
+
+  const [, setTimeoutCallback] = useState(null);
+  const dispatch = useDispatch();
+  const syncLocalAndRemoteReaction = () => {
+    setTimeoutCallback(prev => {
+      if (prev) clearTimeout(prev);
+      return setTimeout(() => {
+        if (likeId && !localReaction) {
+          destroyLike({ id: likeId })(dispatch)
+            .then(() => dispatch(fetchPost(post.id)));
+        } else if (!likeId && localReaction) {
+          createLike({
+            like: {
+              user_id: userId,
+              likeable_id: post.id,
+              likeable_type: 'Post'
+            }
+          })(dispatch)
+            .then(() => dispatch(fetchPost(post.id)))
+        }
+      }, 500);
+    })
+  };
+  useEffect(syncLocalAndRemoteReaction, [localReaction]);
 
   return <aside className="post-likes-and-comments">
     <ul>
       <li
-        className={'post-like-button' + (state.you ? ` post-like-button-clicked-${state.you}` : '')}
+        className={'post-like-button' + (localReaction ? ` post-like-button-clicked-${localReaction}` : '')}
         onClick={toggleLike()}
       >
         <i className="sprite post-like-logo"></i>
