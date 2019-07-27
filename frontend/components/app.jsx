053e467356911193e8e20564bf016f1061c81ef7
yucho <yucho.ho@gmail.com>
2019-07-27 15:12:29 -0700

    Replace HashRouter with BrowserRouter refactor routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Route } from 'react-router-dom';
 import Splash from 'components/splash';
@@ -8,16 +8,19 @@ import Header from 'components/header';
 const App = ({ session, match, history }) => {
   const currentUser = useSelector((state) => state.session.id);
 
-  if (!currentUser) {
-    document.body.classList.remove("logged-in");
-    return <Splash />
-  }
+  useEffect(() => {
+    if (currentUser) {
+      document.body.classList.add("logged-in");
+    } else {
+      document.body.classList.remove("logged-in");
+    }
+    return () => document.body.classList.remove("logged-in");
+  }, [currentUser]);
 
-  document.body.classList.add("logged-in");
   return <>
-    <Header />
+    {currentUser && <Header />}
     <Route path="/" >
-      <NewsFeed />
+      {currentUser ? <NewsFeed /> : <Splash />}
     </Route>
   </>
 };
