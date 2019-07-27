4ded731f50626ddaf1eb47fc2fde02f856ba28d4
yucho <yucho.ho@gmail.com>
2019-07-27 12:24:06 -0700

    Style post form tabs

--- frontend/components/post/post-form-header.jsx
+++ frontend/components/post/post-form-header.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const PostFormHeader = ({ tab, setTab }) => {
   return <header className="post-form-header">
-    <h3 onClick={() => setTab(0)}>
+    <h3 onClick={() => setTab(0)} className={tab === 0 ? 'post-form-tab-active' : ''}>
       <i className="sprite2 create-post-icon" />Create Post
       <Triangle active={tab === 0} />
     </h3>
     <Sep />
-    <h3 onClick={() => setTab(1)}>
+    <h3 onClick={() => setTab(1)} className={tab === 1 ? 'post-form-tab-active' : ''}>
       <i className="sprite2 photo-video-icon" />Photo/Video
       <Triangle active={tab === 1} />
     </h3>
