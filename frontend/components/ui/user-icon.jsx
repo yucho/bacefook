4c96f016bba13bc9729177b03c5530a1a4063807
yucho <yucho.ho@gmail.com>
2019-08-11 22:24:05 -0700

    Bugfix user-icon flicker

--- frontend/components/ui/user-icon.jsx
+++ frontend/components/ui/user-icon.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const UserIcon = ({ className, userId = null }) => {
   const link = !userId ? '/timeline' : `/timeline/${userId}`;
-
   const photoURL = useSelector((state) => {
     const user = state.users[userId];
     if (!user) return null;
@@ -14,10 +13,33 @@ const UserIcon = ({ className, userId = null }) => {
 
     return photo.url;
   });
-  const style = photoURL ? { backgroundImage: `url(${photoURL})` } : {};
+
+  const [state, setState] = useState({
+    className: null,
+    photoURL: null,
+    style: {}
+  });
+  useEffect(() => {
+    setState((prev) => {
+      // Check url before query string
+      const prevURL = /^(.+\..+)\?.*$/.exec(prev.photoURL);
+      const newURL = /^(.+\..+)\?.*$/.exec(photoURL);
+      if (prev.className !== className || prev.photoURL !== photoURL) {
+        if (!!prevURL && !!newURL && prevURL[1] === newURL[1]) {
+          return prev;
+        }
+        const style = photoURL ? { backgroundImage: `url(${photoURL})` } : {};
+        return Object.assign({}, prev, { className, photoURL, style });
+      }
+      return prev;
+    })
+  }, [className, photoURL]);
 
   return <Link to={link}>
-    <div className={'user-icon' + (className ? ` ${className}` : '')} style={style} />
+    <div
+      className={'user-icon' + (state.className ? ` ${state.className}` : '')}
+      style={state.style}
+    />
   </Link>
 };
 
